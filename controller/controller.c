b8a1e2f56502d86b44de7d92f206d12e45750780
William Klock <william.klock@gmail.com>
2016-04-17 22:36:49 -0500

    Added MAXDATASIZE

--- controller/controller.c
+++ controller/controller.c
@@ -16,6 +16,7 @@
 
 #define PORT "3490"  // the port users will be connecting to
 #define BACKLOG 10   // how many pending connections queue will hold
+#define MAXDATASIZE 100
 
 void sigchld_handler(int s) {
     // waitpid() might overwrite errno, so we save and restore it:
@@ -32,7 +33,8 @@ void *get_in_addr(struct sockaddr *sa) {
     return &(((struct sockaddr_in6 *) sa)->sin6_addr);
 }
 
-int main(void) {
+int main(void)
+    int numbytes;
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage their_addr; // connector's address information
