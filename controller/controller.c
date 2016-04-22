15814bf350ad8a04c986a4e1d9122e7d8e7636ef
William Klock <william.klock@gmail.com>
2016-04-21 20:15:11 -0500

    Updated controller code

--- controller/controller.c
+++ controller/controller.c
@@ -1,5 +1,5 @@
 /*
-** controller.c -- a stream socket server demo
+** controller.c -- stream server adapted from Beej's Guide
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,15 +14,11 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "3490"  // the port users will be connecting to
 #define BACKLOG 10   // how many pending connections queue will hold
 #define MAXDATASIZE 100
 
-void sigchld_handler(int s) {
-    // waitpid() might overwrite errno, so we save and restore it:
-    int saved_errno = errno;
-    while (waitpid(-1, NULL, WNOHANG) > 0);
-    errno = saved_errno;
+void close_controller() {
+
 }
 
 // get sockaddr, IPv4 or IPv6:
@@ -32,27 +28,25 @@ void *get_in_addr(struct sockaddr *sa) {
     }
     return &(((struct sockaddr_in6 *) sa)->sin6_addr);
 }
-
-int main(void) {
-    int numbytes;
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+/*
+ * Basic networking code.
+ * Returns a socket file descriptor or -1 if it failed to bind
+ */
+int get_socket(char* port) {
     struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes = 1;
-    char s[INET6_ADDRSTRLEN];
     int rv;
-    char buf[MAXDATASIZE];
-
+    int sockfd;
+    int yes = 1;
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP
     hints.ai_flags = AI_PASSIVE; // use my IP
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+
+    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
+
     // loop through all the results and bind to the first we can
     for (p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
@@ -78,24 +72,39 @@ int main(void) {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
-    if (listen(sockfd, BACKLOG) == -1) {
+    return sockfd;
+
+}
+
+int main(int argc, char** argv) {
+    int numbytes;
+    struct sockaddr_storage their_addr; // connector's address information
+    int new_fd, server_socket;
+    socklen_t sin_size;
+    char s[INET_ADDRSTRLEN];
+    char buf[MAXDATASIZE];
+
+    if(argc != 2) {
+        fprintf(stderr, "\n\t./controller <port>\n\n");
+        exit(1);
+    }
+
+    struct sigaction sig_act;
+    sig_act.sa_handler = close_controller;
+    sigemptyset(&sig_act.sa_mask);
+    sig_act.sa_flags = 0;
+
+    server_socket = get_socket(argv[1]);
+    if (listen(server_socket, BACKLOG) == -1) {
         perror("listen");
         exit(1);
     }
 
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
 
     printf("server: waiting for connections...\n");
     while (1) {  // main accept() loop
         sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
+        new_fd = accept(server_socket, (struct sockaddr *) &their_addr, &sin_size);
         if (new_fd == -1) {
             perror("accept");
             continue;
@@ -105,6 +114,7 @@ int main(void) {
                   s, sizeof s);
 
         printf("server: got connection from %s\n", s);
+
         if (!fork()) { // this is the child process
             if ((numbytes = recv(new_fd, buf, MAXDATASIZE - 1, 0)) == -1) {
                 perror("recv");
@@ -112,16 +122,15 @@ int main(void) {
             } else {
                 buf[numbytes] = '\0';
                 printf("received '%s'\n", buf);
-
             }
 
             if (send(new_fd, "Hello, world!", 13, 0) == -1)
                 perror("send");
+
             close(new_fd);
             exit(0);
 
         }
         close(new_fd);  // parent doesn't need this
     }
-    return 0;
 }
