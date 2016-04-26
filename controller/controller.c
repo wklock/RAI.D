49e7e5f16ab9cd1647d9eb0564533d039e8a7c0a
William Klock <wklock@Thor.local>
2016-04-25 19:35:03 -0500

    cleanup

--- controller/controller.c
+++ controller/controller.c
@@ -13,74 +13,7 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-=======
 #include "controller.h"
-
-int main( int argc, char *argv[] ) {
-   int sockfd, newsockfd, portno, clilen;
-   char buffer[256];
-   struct sockaddr_in serv_addr, cli_addr;
-   int n, pid;
-
-   /* First call to socket() function */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-   if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
-   }
-
-   /* Initialize socket structure */
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   portno = 50000;
-
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_addr.s_addr = INADDR_ANY;
-   serv_addr.sin_port = htons(portno);
-
-   /* Now bind the host address using bind() call.*/
-   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR on binding");
-      exit(1);
-   }
-
-   /* Now start listening for the clients, here
-      * process will go in sleep mode and will wait
-      * for the incoming connection
-   */
-
-   listen(sockfd,5);
-   clilen = sizeof(cli_addr);
-
-   while (1) {
-      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
-      if (newsockfd < 0) {
-         perror("ERROR on accept");
-         exit(1);
-      }
-
-      /* Create child process */
-      pid = fork();
-
-      if (pid < 0) {
-         perror("ERROR on fork");
-         exit(1);
-      }
-
-      if (pid == 0) {
-         /* This is the child process */
-         close(sockfd);
-         doprocessing(newsockfd);
-         exit(0);
-      }
-      else {
-         close(newsockfd);
-      }
-
-   } /* end of while */
-}
->>>>>>> master
 
 #define BACKLOG 10   // how many pending connections queue will hold
 #define MAXDATASIZE 100
